3af571a3692e15fbd009ce13b8696d59ca2517ac
Matheus Baumgart <matheusbaumgart@gmail.com>
2017-10-10 17:44:49 -0300

    Post not found for when post is empty.

--- src/containers/PostPage.js
+++ src/containers/PostPage.js
@@ -5,6 +5,7 @@ import { fetchPost, fetchComments, submitDeletePost, showModal } from '../action
 import AddEditPostModal from '../components/AddEditPostModal'
 import CommentAdd from '../components/comments/CommentAdd'
 import CommentsList from '../components/comments/CommentsList'
+import { Link } from 'react-router-dom'
 
 import { Icon } from 'react-fa'
 import Moment from 'react-moment';
@@ -34,39 +35,48 @@ class PostPage extends Component {
 
     render() {
         const { post, comments } = this.props
+        let hasPost = Object.keys(post).length === 0 && post.constructor === Object;
 
         return (
             <div>
-                <AddEditPostModal />
-
-                <div className="flex space-between">
+                {!hasPost ?
                     <div>
-                        <h2 className="no-margin-bottom">{post.title}</h2>
-                        <small>by {post.author} - <Moment fromNow>{post.timestamp}</Moment></small>
-                    </div>
+                        <AddEditPostModal />
 
+                        <div className="flex space-between">
+                            <div>
+                                <h2 className="no-margin-bottom">{post.title}</h2>
+                                <small>by {post.author} - <Moment fromNow>{post.timestamp}</Moment></small>
+                            </div>
+
+                            <div>
+                                <Icon name="pencil" className="post-action-link" onClick={this.openModal} />
+                                <Icon name="remove" className="post-action-link" onClick={this.deletePost} />
+                            </div>
+                        </div>
+
+                        <br />
+
+                        <p>{post.body}</p>
+
+                        <br />
+
+                        <Score post={post} />
+
+                        <hr />
+
+                        <CommentAdd postID={post.id} />
+
+                        <hr />
+
+                        <CommentsList comments={comments.items} />
+                    </div>
+                    :
                     <div>
-                        <Icon name="pencil" className="post-action-link" onClick={this.openModal} />
-                        <Icon name="remove" className="post-action-link" onClick={this.deletePost} />
+                        <h2>Post not found.</h2> 
+                        <br />Return to <Link to='/'>home</Link>
                     </div>
-                </div>
-
-                <br />
-
-                <p>{post.body}</p>
-
-                <br />
-
-                <Score post={post} />
-
-                <hr />
-
-                <CommentAdd postID={post.id} />
-
-                <hr />  
-
-                <CommentsList comments={comments.items} />          
-               
+                }
             </div>
         )
     }
